642f3e420566130a53357f26d48d7f64ebdaa7ef
sltiedeman <sltiedeman@gmail.com>
2015-11-11 16:11:23 -0500

    Pulling more videos from API

--- controller.js
+++ controller.js
@@ -1,34 +1,94 @@
 
 var queryTerm;
 var videosBySamePoster = [];
-angular.module('myApp', []).controller('myController', function($scope, $http){
+var otherVideos = [];
+var apiKey = 'AIzaSyB4cvmIQgRWdLIHTUm_L3-KB4p3O62mTKY';
+var baseUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=4&q=';
+var youTubeUrl = 'https://www.youtube.com/watch?v=';
+
+
+angular.module('myApp', []).controller('myController', function($scope, $sce, $http){
+	$scope.mainUrl = $sce.trustAsResourceUrl('https://www.youtube.com/embed/RUz_EXSmp9w');
 	var videosBySamePoster = [];
 
 	// var singleVideoUrl = 'https://www.googleapis.com/youtube/v3/videos?id='+videoId+'&part=snippet,statistics&key='+apiKey;
-	$scope.videos = ""
+	$scope.videos = "";
+
+	moreVideosUrl = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=8&key=';
+	moreVideosUrl += 'AIzaSyB4cvmIQgRWdLIHTUm_L3-KB4p3O62mTKY';
+	$http.get(moreVideosUrl).success(function(data){
+		$scope.popularVideos = data.items;
+		console.log($scope.popularVideos);
+		$scope.videoList = [];
+		for(i=0; i < 8; i++){
+			var videoTitle = $scope.popularVideos[i].snippet.title;
+			if(videoTitle.length > 55){
+					var newTitle = videoTitle.slice(0, 54) + "...";	
+				}else{
+					var newTitle = videoTitle;
+				}
+			var videos = {
+				vidtitle: newTitle,
+				thumb: $scope.popularVideos[i].snippet.thumbnails.default.url,
+				url: youTubeUrl + $scope.popularVideos[i].id,
+				postedBy: $scope.popularVideos[i].snippet.channelTitle,
+			}
+			$scope.videoList.push(videos);
+		}
+		console.log($scope.videoList);
+
+	})
+
+
+
 	$scope.addVideo = function(){
+		//building the url to get the data object
 		queryTerm = $scope.videoSearch;
-		var apiKey = 'AIzaSyB4cvmIQgRWdLIHTUm_L3-KB4p3O62mTKY';
-		var baseUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=';
 		var searchUrl = queryTerm;
 		var endUrl = '&key=';
 		var totalUrl = baseUrl + searchUrl + endUrl + apiKey;
+		videosBySamePoster = [];
+
+		//run this function when user submits a search
 		$http.get(totalUrl).success(function(data) {
+			var videohtml = "";
 	      	$scope.videos = data.items;
-	      	for(i=0; i<3; i++){
-				var youTubeUrl = 'https://www.youtube.com/watch?v=';
+	      	//this is the main video to go on the page
+	      	var makeMainUrl = "https://www.youtube.com/embed/" + $scope.videos[0].id.videoId;
+	      	var mainVideoHtml = '<iframe width="100%" height="300px" src="' + makeMainUrl + '" ';
+	      	mainVideoHtml += 'frameborder="0" allowfullscreen></iframe>';
+	      	mainVideoHtml += '<p></p><span>' + $scope.videos[0].snippet.title + '</span>';
+	      	mainVideoHtml += '<p>by ' + $scope.videos[0].snippet.channelTitle + '</p';
+	      	$('#video').html(mainVideoHtml);
+
+	      	console.log($scope.videos);
+	      	for(i=1; i<4; i++){	      		
 				var videos = {
 					vidtitle: $scope.videos[i].snippet.title,
-					thumb: $scope.videos[i].snippet.thumbnails.url,
+					thumb: $scope.videos[i].snippet.thumbnails.default.url,
 					url: youTubeUrl + $scope.videos[i].id.videoId,
 					duration: "2:43:58",
-					postedBy: "Stephen Tiedemann",
+					postedBy: $scope.videos[i].snippet.channelTitle,
 					totalViews: 100000
 				}
+				console.log(videos.url);
 				videosBySamePoster.push(videos);
 			}
-			populatePage();
-
+			console.log(videosBySamePoster);
+			for(i=0; i<videosBySamePoster.length; i++){
+			var videoTitle = videosBySamePoster[i].vidtitle;
+			if(videoTitle.length > 50){
+				var newTitle = videoTitle.slice(0, 49) + "...";	
+			}else{
+				var newTitle = videoTitle;
+			}
+			videohtml +='<div class="thumbnail"><a href=' + videosBySamePoster[i].url + '><img src=' + videosBySamePoster[i].thumb + '></a></div>';
+			videohtml +='<div class="feature-minis"' + ' id="feature-minis-"' + i + '><span class="video-title">' + newTitle + '</span>';
+			videohtml += '<p>by ' + videosBySamePoster[i].postedBy + '</p>';
+			videohtml += '<p>Duration: ' + videosBySamePoster[i].duration + '</p>';
+			videohtml += '<p>Views: ' + videosBySamePoster[i].totalViews + '<p></div>';
+	}
+	$('#featured-previews').html(videohtml);
     	});
 
 	}
@@ -37,65 +97,14 @@ angular.module('myApp', []).controller('myController', function($scope, $http){
 	
 
 
-var otherVideos = [
-	{
-	    vidtitle: "The Best of Rachmaninoff",
-	    thumb: "https://i.ytimg.com/vi/vpaPWuDQUcc/default.jpg",
-	    url: "https://www.youtube.com/watch?v=vpaPWuDQUcc",
-	    duration: "2:43:58",
-	    postedBy: "Classical Music11",
-	    totalViews: 857721
-	}
-];
 
 
-// var videosBySamePoster = [
-// 	{
-// 	    vidtitle: "StarCraft II: Legacy of the Void Opening Cinematic",
-// 	    thumb: "https://i.ytimg.com/vi_webp/M_XwzBMTJaM/default.webp",
-// 	    url: "https://www.youtube.com/watch?v=M_XwzBMTJaM",
-// 	    duration: "29:51",
-// 	    postedBy: "StarCraft",
-// 	    totalViews: 3927195
-// 	}
-// ];
 
-var videohtml="";
+	
 
-$(document).ready(function(){
-	for(i=0; i<videosBySamePoster.length; i++){
-		var videoTitle = videosBySamePoster[i].vidtitle;
-		if(videoTitle.length > 50){
-			var newTitle = videoTitle.slice(0, 49) + "...";	
-		}else{
-			var newTitle = videoTitle;
-		}
-		videohtml +='<div class="thumbnail"><a href=' + videosBySamePoster[i].url + '><img src=' + videosBySamePoster[i].thumb + '></a></div>';
-		videohtml +='<div class="feature-minis"' + ' id="feature-minis-"' + i + '><span class="video-title">' + newTitle + '</span>';
-		videohtml += '<p>by ' + videosBySamePoster[i].postedBy + '</p>';
-		videohtml += '<p>Duration: ' + videosBySamePoster[i].duration + '</p>';
-		videohtml += '<p>Views: ' + videosBySamePoster[i].totalViews + '<p></div>';
-	}
-	$('#featured-previews').html(videohtml);
+	
 
-	addVideos();
 
-function addVideos(){
-	videohtml="";
-	for(i=0; i<otherVideos.length; i++){
-		var videoTitle = otherVideos[i].vidtitle;
-		if(videoTitle.length > 55){
-			var newTitle = videoTitle.slice(0, 54) + "...";	
-		}else{
-			var newTitle = videoTitle;
-		}
-		videohtml += '<div class = "more-videos-previews"><div class ="more-thumbnails"><a href=' + otherVideos[i].url + '><img src=';
-		videohtml += otherVideos[i].thumb + '></a></div><div class="thumbnail-text">';
-		videohtml += '<p class="video-title">' + newTitle + '</p><p>' + otherVideos[i].postedBy + '</p></div></div>';
-
-	}
-	$('#more-videos').html(videohtml);
-}
 
 
 	$('#form-submit').submit(function(){
@@ -117,7 +126,6 @@ function addVideos(){
 
 
 
-})
 
 
 
